1aead18a2590ee9bb5dc36177132b7479e220eb1
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-16 16:52:34 +0800

    Create destroy_post.rb

---
+++ app/jobs/regular/destroy_post.rb
@@ -0,0 +1,9 @@
+module DiscourseStaticPagesSync
+  class DestroyPost < ::Regular
+    def execute(args)
+      post_type = args.post_type
+      author = args.post.author.username
+      id = args.post.id
+    end
+  end
+end
