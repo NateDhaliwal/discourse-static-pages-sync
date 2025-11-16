5d3252f84dd464fd84965d3f336ab6ae9a8ac081
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-16 16:17:53 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
+# name: discourse-static-pages-sync
 # about: TODO
 # meta_topic_id: TODO
 # version: 0.0.1
-# authors: Discourse
+# authors: NateDhaliwal
 # url: TODO
 # required_version: 2.7.0
 
 enabled_site_setting :plugin_name_enabled
 
-module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+module ::DiscourseStaticPagesSync
+  PLUGIN_NAME = "discourse-static-pages-sync"
 end
 
-require_relative "lib/my_plugin_module/engine"
+require_relative "lib/discourse_static_pages_sync/engine"
 
 after_initialize do
   # Code which should run after Rails has finished booting
