ce01cadc590b8840db869e921faa8c1e28d19a6f
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-17 12:00:52 +0800

    Add file paths of jobs

--- plugin.rb
+++ plugin.rb
@@ -17,17 +17,22 @@ end
 require_relative "lib/discourse_static_pages_sync/engine"
 
 after_initialize do
+  %w[
+    ../app/jobs/regular/create_post_and_sync.rb
+    ../app/jobs/regular/destroy_post_and_sync.rb
+  ].each { |path| require File.expand_path(path, __FILE__) }
+  
   on(:topic_created) do |topic|
     Jobs.enqueue(
-      :create_post,
+      :create_post_and_sync,
       post_type: "topic",
       post: topic
     )
   end
 
-  on(:post_created) do post
+  on(:post_created) do |post|
     Jobs.enqueue(
-      :create_post,
+      :create_post_and_sync,
       post_type: "post",
       post: post
     )
@@ -35,15 +40,15 @@ after_initialize do
 
   on(:topic_destroyed) do |topic|
     Jobs.enqueue(
-      :destroy_post,
+      :destroy_post_and_sync,
       post_type: "topic",
       post: topic
     )
   end
 
-  on(:post_destroyed) do post
+  on(:post_destroyed) do |post|
     Jobs.enqueue(
-      :destroy_post,
+      :destroy_post_and_sync,
       post_type: "post",
       post: post
     )
