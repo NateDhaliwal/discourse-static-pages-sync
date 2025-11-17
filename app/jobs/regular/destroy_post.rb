3b739d39095b0ef200fb054024d002d45b0911db
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-17 11:33:55 +0800

    Use [:key_name] notation

--- app/jobs/regular/destroy_post.rb
+++ app/jobs/regular/destroy_post.rb
@@ -1,9 +1,9 @@
 module DiscourseStaticPagesSync
   class DestroyPost < ::Jobs::Regular
     def execute(args)
-      post_type = args.post_type
-      author = args.post.author.username
-      id = args.post.id
+      post_type = args[:post_type]
+      author = args[:post][:author][:username]
+      id = post_type == "topic" ? args[:post][:id] : args[:post][:topic_id]
     end
   end
 end
