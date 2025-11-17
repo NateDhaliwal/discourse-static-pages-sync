9f660e85dd4a75a61cfe522424daf970bc4487bf
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-17 16:05:28 +0800

    Add content and finish up creation of topics and posts

--- app/jobs/regular/create_post_and_sync.rb
+++ app/jobs/regular/create_post_and_sync.rb
@@ -13,11 +13,62 @@ module ::Jobs
 
       topic_id = post_type == "topic" ? args[:id] : args[:topic_id]
       topic_name = Topic.find_by(id: topic_id).name
-      cooked = args[:cooked]
-      cooked_encoded = Base64.encode64(cooked) # Github needs text in Base64
+
       created_at = args[:created_at]
       updated_at = args[:updated_at]
       whisper = args[:whisper]
+      
+      cooked = args[:cooked]
+      content = ""
+
+      if post_type == "topic" then
+        content = SiteSetting.file_template.sub(
+          "@{post_type}",
+          post_type
+        ).sub!(
+          "@{topic_name}",
+          topic_name
+        ).sub!(
+          "@{topic_id}",
+          topic_id
+        ).sub!(
+          "@{category_name}",
+          category_name
+        ).sub!(
+          "@{created_at}",
+          created_at
+        ).sub!(
+          "@{updated_at}",
+          updated_at
+        )
+        # ).sub!(
+        #   "@{whisper}",
+        #   whisper
+        # )
+      else
+        content = SiteSetting.file_template.sub(
+          "@{post_type}",
+          post_type
+        ).sub!(
+          "@{topic_name}",
+          topic_name
+        ).sub!(
+          "@{topic_id}",
+          topic_id
+        ).sub!(
+          "@{created_at}",
+          created_at
+        ).sub!(
+          "@{updated_at}",
+          updated_at
+        ).sub!(
+          "@{whisper}",
+          whisper
+        )
+      end
+
+      content += "\n\n" + cooked
+      content_encoded = Base64.encode64(content) # Github needs text in Base64
 
       file_path = post_type == "topic" ? SiteSetting.topic_post_path : SiteSetting.reply_post_path
       file_path.sub!("@{category}", category_name)
@@ -33,11 +84,28 @@ module ::Jobs
       )
 
       req_body = {
-        :message => cooked_encoded,
-        
+        :message => SiteSetting.commit_message,
+        :committer => {
+          "name": SiteSetting.github_committer_username,
+          "email": SiteSetting.github_committer_email
+        },
+        :content => cooked_encoded,
       }
 
-      resp = conn.put("/repos/#{SiteSetting.github_committer_username}/#{SiteSetting.github_repo_name}/contents/#{file_path}", 
+      json_req_body = JSON.generate(req_body)
+
+      target_repo = SiteSetting.target_github_repo
+      repo_user = target_repo.split("https://github.com/")[1].split("/")[0]
+      repo_name =target_repo.split("https://github.com/")[1].split("/")[1]
+      
+      resp = conn.put(
+        "/repos/#{repo_user}/#{repo_name}/contents/#{file_path}",
+        json_req_body
+      )
+
+      if resp.status == 200 and SiteSetting.log_when_post_synced then
+        Rails.logger.info "Topic '#{topic_name}' has been uploaded"
+      end
     end
   end
 end
