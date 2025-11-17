074ece8c212e8371168630b26544817302f732fb
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-17 12:35:58 +0800

    Add .username

--- app/jobs/regular/destroy_post_and_sync.rb
+++ app/jobs/regular/destroy_post_and_sync.rb
@@ -2,7 +2,7 @@ module ::Jobs
   class DestroyPostAndSync < ::Jobs::Base
     def execute(args)
       post_type = args[:post_type]
-      username = User.find_by(id: args[:user_id])
+      username = User.find_by(id: args[:user_id]).username
       puts username
       puts args
     end
