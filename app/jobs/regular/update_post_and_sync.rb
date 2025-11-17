31b635a02b75c2e78e405307dc30e06de524ee1e
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-17 12:47:50 +0800

    Update update_post_and_sync.rb

--- app/jobs/regular/update_post_and_sync.rb
+++ app/jobs/regular/update_post_and_sync.rb
@@ -1,1 +1,10 @@
-
+module ::Jobs
+  class UpdatePostAndSync < ::Jobs::Base
+    def execute(args)
+      post_type = args[:post_type]
+      username = User.find_by(id: args[:user_id]).username
+      puts username
+      puts args
+    end
+  end
+end
