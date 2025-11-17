d8e983e714d775a7683ac27bcafa754d1ab8b385
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-17 11:34:42 +0800

    Debug: Log post

--- app/jobs/regular/destroy_post.rb
+++ app/jobs/regular/destroy_post.rb
@@ -4,6 +4,8 @@ module DiscourseStaticPagesSync
       post_type = args[:post_type]
       author = args[:post][:author][:username]
       id = post_type == "topic" ? args[:post][:id] : args[:post][:topic_id]
+      puts args[:post_type]
+      puts args[:post]
     end
   end
 end
