fab767925983714e759ff5b31e5310b21afae021
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-17 17:04:32 +0800

    Add logic to combine uploading and updating

--- app/jobs/regular/create_post_and_sync.rb
+++ app/jobs/regular/create_post_and_sync.rb
@@ -8,6 +8,21 @@ module ::Jobs
   class CreatePostAndSync < ::Jobs::Base
     def execute(args)
       post_type = args[:post_type]
+      operation = args[:operation]
+
+      target_repo = SiteSetting.target_github_repo
+      repo_user = target_repo.split("https://github.com/")[1].split("/")[0]
+      repo_name = target_repo.split("https://github.com/")[1].split("/")[1]
+
+      conn = Faraday.new(
+        url: "http://api.github.com",
+        headers: {
+          "Accept" => "application/vnd.github+json",
+          "Authorization" => SiteSetting.github_access_token,
+          "X-GitHub-Api-Version" => "2022-11-28"
+        }
+      )
+      
       username = User.find_by(id: args[:user_id]).username
       category_name = post_type == "topic" ? Category.find_by(id: args[:category_id]).name : nil
 
@@ -21,8 +36,22 @@ module ::Jobs
       cooked = args[:cooked]
       content = ""
 
+      sha = ""
+      if operation == "update" do
+        passed = false
+        resp = ""
+        while !passed then
+          resp = conn.get("/repos/#{repo_user}/#{repo_name}/contents/#{file_path}")
+          if resp.status == 200:
+            passed = true
+            break
+          end
+        end
+        sha = resp.body[:sha]
+      end
+
       if post_type == "topic" then
-        content = SiteSetting.topic_post_template.sub(
+        content = SiteSetting.topic_post_template.sub!(
           "@{post_type}",
           post_type
         ).sub!(
@@ -46,7 +75,7 @@ module ::Jobs
         #   whisper
         # )
       else
-        content = SiteSetting.reply_post_template.sub(
+        content = SiteSetting.reply_post_template.sub!(
           "@{post_type}",
           post_type
         ).sub!(
@@ -66,47 +95,46 @@ module ::Jobs
           whisper
         )
       end
-
-      content += "\n\n" + cooked
+      
+      content.sub!("@{content}", cooked)
       content_encoded = Base64.encode64(content) # Github needs text in Base64
 
       file_path = post_type == "topic" ? SiteSetting.topic_post_path : SiteSetting.reply_post_path
       file_path.sub!("@{category}", category_name)
       file_path.sub!("@{topic_name}", topic_name)
 
-      conn = Faraday.new(
-        url: "http://api.github.com",
-        headers: {
-          "Accept" => "application/vnd.github+json",
-          "Authorization" => SiteSetting.github_access_token,
-          "X-GitHub-Api-Version" => "2022-11-28"
+      req_body = {}
+      if operation == "create" then
+        req_body = {
+          :message => SiteSetting.commit_message,
+          :committer => {
+            "name": SiteSetting.github_committer_username,
+            "email": SiteSetting.github_committer_email
+          },
+          :content => cooked_encoded,
         }
-      )
-
-      req_body = {
-        :message => SiteSetting.commit_message,
-        :committer => {
-          "name": SiteSetting.github_committer_username,
-          "email": SiteSetting.github_committer_email
-        },
-        :content => cooked_encoded,
-      }
+      else
+        req_body = {
+          :message => SiteSetting.commit_message,
+          :committer => {
+            "name": SiteSetting.github_committer_username,
+            "email": SiteSetting.github_committer_email
+          },
+          :sha => sha,
+        }
+      end
 
       json_req_body = JSON.generate(req_body)
-
-      target_repo = SiteSetting.target_github_repo
-      repo_user = target_repo.split("https://github.com/")[1].split("/")[0]
-      repo_name =target_repo.split("https://github.com/")[1].split("/")[1]
       
       resp = conn.put(
         "/repos/#{repo_user}/#{repo_name}/contents/#{file_path}",
         json_req_body
       )
 
-      if (resp.status == 200 or resp.status == 201) and SiteSetting.log_when_post_synced then
-        Rails.logger.info "Topic '#{topic_name}' has been uploaded"
+      if (resp.status == 200 or resp.status == 201) and SiteSetting.log_when_post_uploaded then
+        Rails.logger.info "Topic '#{topic_name}' has been #{operation == "create" ? "uploaded" : "updated"}"
       elsif resp.status == 422 or resp.status == 403 then # Job failed
-        Rails.logger.error "An error occurred when trying to upload '#{topic_name}': #{resp.body}"
+        Rails.logger.error "An error occurred when trying to upload or update '#{topic_name}': #{resp.body}"
         if resp.headers["x-ratelimit-remaining"].to_i == 0: # Rate limit reached
           time_reset = Time.at(1659645535)
           time_now = Time.now()
