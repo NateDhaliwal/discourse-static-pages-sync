8651c518d16adb18df0502fdc225cc8c0b8d464b
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-17 12:22:40 +0800

    Update destroy_post_and_sync.rb

--- app/jobs/regular/destroy_post_and_sync.rb
+++ app/jobs/regular/destroy_post_and_sync.rb
@@ -2,10 +2,9 @@ module ::Jobs
   class DestroyPostAndSync < ::Jobs::Base
     def execute(args)
       post_type = args[:post_type]
-      author = args[:post][:author][:username]
-      id = post_type == "topic" ? args[:post][:id] : args[:post][:topic_id]
-      puts args[:post_type]
-      puts args[:post]
+      username = User.find_by(id: args[:user_id])
+      puts username
+      puts args
     end
   end
 end
