52ee82b012e47508b5433a8519473ad723d6627d
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-17 11:30:03 +0800

    Add ::Jobs

--- app/jobs/regular/destroy_post.rb
+++ app/jobs/regular/destroy_post.rb
@@ -1,5 +1,5 @@
 module DiscourseStaticPagesSync
-  class DestroyPost < ::Regular
+  class DestroyPost < ::Jobs::Regular
     def execute(args)
       post_type = args.post_type
       author = args.post.author.username
