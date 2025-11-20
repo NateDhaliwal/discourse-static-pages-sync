eaf1afda595e47a68bcb86e936eecfae0ff285e9
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-20 16:20:45 +0800

    Create synced_topics_backfill table

---
+++ app/models/discourse_static_pages_sync/synced_topics_backfill.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module ::DiscourseChatbot
+  class TopicEmbeddingsBookmark < ActiveRecord::Base
+    self.table_name = 'synced_topics_backfill'
+
+    validates :topic_id, presence: true
+  end
+end
+
+# == Schema Information
+#
+# Table name: synced_topics_backfill
+#
+#  id         :bigint           not null, primary key
+#  topic_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
---
+++ db/migrate/20251120080625_create_synced_topics_backfill.rb
@@ -0,0 +1,9 @@
+class CreateSyncedTopicsBackfill < ActiveRecord::Migration[8.0]
+  def change
+    create_table :synced_topics_backfill do |t|
+      t.integer :topic_id
+
+      t.timestamps
+    end
+  end
+end
