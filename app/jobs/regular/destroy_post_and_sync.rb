3dd9c91dc425459ec6791f2f99071edb058a7c95
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-28 17:19:55 +0800

    Don't use dot notation for post_edits

--- app/jobs/regular/destroy_post_and_sync.rb
+++ app/jobs/regular/destroy_post_and_sync.rb
@@ -112,14 +112,14 @@ module ::Jobs
       
       if !post_edits["errors"] && operation == "edit_topic" then
         old_category_slug = category_slug
-        if post_edits.category_changes then
-          old_category_slug = Category.find_by(id: post_edits.category_changes.previous).slug
+        if post_edits["category_changes"] then
+          old_category_slug = Category.find_by(id: post_edits["category_changes"]["previous"]).slug
         end
         old_file_path = post_type == "topic" ? SiteSetting.topic_post_path : SiteSetting.reply_post_path.sub("@{post_number}", post_number.to_s)
         if old_file_path.include? "@{category_slug}" then
           old_file_path = old_file_path.sub("@{category_slug}", old_category_slug)
         end
-        old_topic_title = post_edits.title_changes.side_by_side.split('<div class=\"revision-content\"><div>')[1].split('</div></div><div class=\"revision-content\">')[0].split("</div></div>")[0].sub("<del>", "").sub("</del>", "")
+        old_topic_title = post_edits["title_changes"]["side_by_side"].split('<div class=\"revision-content\"><div>')[1].split('</div></div><div class=\"revision-content\">')[0].split("</div></div>")[0].sub("<del>", "").sub("</del>", "")
         # Discourse's in-built Slug
         old_topic_slug = Slug.for(old_topic_title)
         if old_file_path.include? "@{topic_slug}" then
